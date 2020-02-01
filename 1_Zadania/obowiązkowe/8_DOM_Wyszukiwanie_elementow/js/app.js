12c67ab012fd4c85f50ae65366023c0f3f8c73b4
mr-craftsman <radesign@op.pl>
2020-02-01 13:11:06 +0100

    Add files via upload

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -1,6 +1,39 @@
-document.addEventListener("DOMContentLoaded", function () {
-    /*
-    Poniżej napisz kod rozwiązujący zadania. Odpowiedzi oddzielaj komentarzami.
-     */
 
+document.addEventListener("DOMContentLoaded", function () {
+
+//zadanie 0
+var element = document.querySelector(".title");
+
+function getDataAnimation(element){
+    var dataAnimation = element.getAttribute("data-animation");
+    return dataAnimation;
+}
+console.log(getDataAnimation(element));
+
+//zadanie 1
+var idHomeOne = document.getElementById("home");
+var idHomeTwo = document.querySelector("#home");
+var liFirst = document.querySelector("li[data-direction]");
+var classBlock = document.querySelector(".block");
+/* testing
+console.log(idHomeOne);
+console.log(idHomeTwo);
+console.log(liFirst);
+console.log(classBlock);
+*/
+
+//zadanie 2
+var liNav = document.querySelectorAll("nav li");
+var divParagraphs = document.querySelectorAll("div p");
+var divArticle = document.querySelectorAll("article div");
+/* testing
+console.log(liNav);
+console.log(divParagraphs);
+console.log(divArticle);
+*/
+
+//zadanie 3
+var firstArticle = document.querySelector("article.first");
+var headingOne = firstArticle.querySelectorAll("h1");
+console.log(headingOne.length);
 });
